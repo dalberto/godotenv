260beca273dac53395c9614bfa2cb713688ea476
Darlin <Darlin@SinglePlatform.com>
2014-07-23 17:06:21 -0400

    Add "skipMissing" argument to "Load" for skipping files that error out.

--- godotenv.go
+++ godotenv.go
@@ -1,5 +1,5 @@
 /*
-A go port of the ruby dotenv library (https://github.com/bkeepers/dotenv)
+Package godotenv is a Go port of the Ruby dotenv library (https://github.com/bkeepers/dotenv)
 
 Examples/readme can be found on the github page at https://github.com/joho/godotenv
 
@@ -23,21 +23,32 @@ import (
 )
 
 /*
-	Call this function as close as possible to the start of your program (ideally in main)
 
-	If you call Load without any args it will default to loading .env in the current path
+Load reads and parses the files specified, adding environment variables it finds along the way.
 
-	You can otherwise tell it which files to load (there can be more than one) like
+Call this function as close as possible to the start of your program (ideally in main).
 
-		godotenv.Load("fileone", "filetwo")
+If you call Load without any args it will default to loading ".env" in the current path.
 
-	It's important to note that it WILL NOT OVERRIDE an env variable that already exists - consider the .env file to set dev vars or sensible defaults
+You can otherwise tell it which files to load (there can be more than one) like:
+
+	godotenv.Load("fileone", "filetwo")
+
+It's important to note that it WILL NOT OVERRIDE an env variable that already exists - consider the .env file to set dev vars or sensible defaults.
+
+When skipMissing is set to `true`, missing files or files that can't be read because of permissions will simply be skipped.
+
 */
-func Load(filenames ...string) (err error) {
+func Load(skipMissing bool, filenames ...string) (err error) {
 	filenames = filenamesOrDefault(filenames)
 
 	for _, filename := range filenames {
 		err = loadFile(filename)
+
+		if skipMissing && (err == os.ErrNotExist || err == os.ErrPermission) {
+			continue
+		}
+
 		if err != nil {
 			return // return early on a spazout
 		}
@@ -45,6 +56,8 @@ func Load(filenames ...string) (err error) {
 	return
 }
 
+// Read reads the filenames specified, but instead of setting environment variables based on the contents of the files,
+// returns a map of environment variables and their values.
 func Read(filenames ...string) (envMap map[string]string, err error) {
 	filenames = filenamesOrDefault(filenames)
 	envMap = make(map[string]string)
@@ -68,13 +81,14 @@ func Read(filenames ...string) (envMap map[string]string, err error) {
 func filenamesOrDefault(filenames []string) []string {
 	if len(filenames) == 0 {
 		return []string{".env"}
-	} else {
-		return filenames
 	}
+
+	return filenames
 }
 
 func loadFile(filename string) (err error) {
 	envMap, err := readFile(filename)
+
 	if err != nil {
 		return
 	}
@@ -88,6 +102,7 @@ func loadFile(filename string) (err error) {
 
 func readFile(filename string) (envMap map[string]string, err error) {
 	file, err := os.Open(filename)
+
 	if err != nil {
 		return
 	}
@@ -96,6 +111,7 @@ func readFile(filename string) (envMap map[string]string, err error) {
 	envMap = make(map[string]string)
 
 	var lines []string
+
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		lines = append(lines, scanner.Text())
@@ -121,9 +137,11 @@ func parseLine(line string) (key string, value string, err error) {
 
 	// ditch the comments (but keep quoted hashes)
 	if strings.Contains(line, "#") {
+		var segmentsToKeep []string
+
 		segmentsBetweenHashes := strings.Split(line, "#")
 		quotesAreOpen := false
-		segmentsToKeep := make([]string, 0)
+
 		for _, segment := range segmentsBetweenHashes {
 			if strings.Count(segment, "\"") == 1 || strings.Count(segment, "'") == 1 {
 				if quotesAreOpen {
@@ -151,12 +169,13 @@ func parseLine(line string) (key string, value string, err error) {
 	}
 
 	if len(splitString) != 2 {
-		err = errors.New("Can't separate key from value")
+		err = errors.New("parse line: can't separate key from value")
 		return
 	}
 
 	// Parse the key
 	key = splitString[0]
+
 	if strings.HasPrefix(key, "export") {
 		key = strings.TrimPrefix(key, "export")
 	}
@@ -183,5 +202,6 @@ func parseLine(line string) (key string, value string, err error) {
 
 func isIgnoredLine(line string) bool {
 	trimmedLine := strings.Trim(line, " \n\t")
+
 	return len(trimmedLine) == 0 || strings.HasPrefix(trimmedLine, "#")
 }
